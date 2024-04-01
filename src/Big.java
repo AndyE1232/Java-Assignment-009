9ec342bff7f2d769bc93bdaee1fca9abe140f584
AndrewE <andrewescarcega04@gmail.com>
2024-03-31 20:30:22 -0700

    Completed Assignment 009

--- src/Big.java
+++ src/Big.java
@@ -1,5 +1,25 @@
+import java.math.BigInteger;
 public class Big {
+
+public static BigInteger factorial(int n) {
+
+    BigInteger fact = BigInteger.valueOf(1);
+    for (int i = 0; i <= n; i++) {
+        if (i == 0){
+            System.out.println("0 1");
+        } else {
+            fact = fact.multiply(BigInteger.valueOf(i));
+            System.out.println(i + " " + fact);
+        }
+    }
+    return fact;
+
+}
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    factorial(30);
+
     }
 }
